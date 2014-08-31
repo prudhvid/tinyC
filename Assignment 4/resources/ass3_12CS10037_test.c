8be8b154389d607a2b586109e3e3e60f6a8ff9e2
prudhvid <dharmana.prudhvi@gmail.com>
2014-08-31 23:16:54 +0530

    assignment 4 done

---
+++ Assignment 4/resources/ass3_12CS10037_test.c
@@ -0,0 +1,22 @@
+int a,b,c;
+
+int x;
+int a,v,cd;
+
+
+int main(int argc, char const *argv[])
+{
+	if(a==b)
+		return 1;
+	for ( i = 0; i < 1000; ++i){
+		for ( i = 0; i < 2938; ++i){
+			if(a==b*293+292*n)
+				break;
+		}
+	}
+
+	while(i==0){}
+
+	i++;
+	return 0;
+}
---
+++ Assignment 4/resources/myfile.c
@@ -0,0 +1,69 @@
+#include "stdio.h"
+#include "y.tab.h"
+#include <iostream>
+extern int yylex();
+extern int yyparse();
+extern char* yytext;
+void yyerror(char *s);
+
+
+void yyerror(char* s)
+{
+	printf("%s",s);
+}
+int main()
+{
+	freopen("ass3_12CS10037_test.c", "r", stdin);
+	int token;
+	std::string s;
+	// while((token=yylex())){
+	// 	switch(token){
+	// 		case 0:s="KW "+std::string(yytext);
+	// 		break;
+	// 		case IDENTIFIER:s="ID "+std::string(yytext);
+	// 		break;
+	// 		case STRING_LITERAL:s="SL "+std::string(yytext);
+	// 		break;
+			
+			
+	// 		case SIZEOF: s="SIZEOF";
+	// 		break;
+	// 		case ENUM: s="ENUM";
+	// 		break;
+	// 		case STATIC: s="STATIC";
+	// 		break;
+	// 		case CASE: s="CASE";
+	// 		break;
+	// 		case DEFAULT: s="DEFAULT";
+	// 		break;
+	// 		case IF: s="IF";
+	// 		break;
+	// 		case ELSE: s="ELSE";
+	// 		break;
+	// 		case SWITCH: s="SWITCH";
+	// 		break;
+	// 		case WHILE: s="WHILE";
+	// 		break;
+	// 		case DO: s="DO";
+	// 		break;
+	// 		case FOR: s="FOR";
+	// 		break;
+	// 		case GOTO: s="GOTO";
+	// 		break;
+	// 		case CONTINUE: s="CONTINUE";
+	// 		break;
+	// 		case BREAK: s="BREAK";
+	// 		break;
+	// 		case RETURN: s="RETURN";
+	// 		break;
+	// 		case CONSTANT: s="CONSTANT";
+	// 		break;
+	// 		default: s=yytext;
+	// 		// printf("token value=%d",token);
+	// 	}
+	// 	printf("< %s > ",s.c_str());
+	// }
+	// printf("%s\n", yytext);
+	yyparse();
+	return 0;
+}
